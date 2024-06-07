6b9d4fcc0a0081b19f97be6b27dbc89ace7231d3
Sarah Borracci <sarahborracci@outlook.com>
2024-06-07 17:18:33 +1000

    correct chema url for codegen.ts

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "https://my-apollo-server.onrender.com/graphql",
+  schema: "https://my-grid-portfolio-client.onrender.com",
   documents: ["src/**/*.tsx"],
   generates: {
     "./src/__generated__/": {
