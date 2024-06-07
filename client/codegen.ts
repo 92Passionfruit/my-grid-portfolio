a7c1e54d090f1de434eb0f4303bdd2a8f319c95d
Sarah Borracci <sarahborracci@outlook.com>
2024-06-07 17:15:54 +1000

    change codegen.ts schema to render url that server is deployed on + add --config codegen.ts generate script in package.json (client)

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "http://localhost:4000",
+  schema: "https://my-apollo-server.onrender.com/graphql",
   documents: ["src/**/*.tsx"],
   generates: {
     "./src/__generated__/": {
